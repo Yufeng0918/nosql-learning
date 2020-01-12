c3c634396db3b1d086492285a4b4ccfa17cdc57e
daiyu <jiangyufeng.0918@gmail.com>
2020-01-12 22:23:15 +0800

    init redis learning

---
+++ redis-java/src/test/java/com/yufeng/redis/TestAPI.java
@@ -0,0 +1,22 @@
+package com.yufeng.redis;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
+public class TestAPI {
+	public static void main(String[] args) 
+	{
+		Jedis jedis = new Jedis("127.0.0.1",6379);
+		
+		jedis.set("k1","v1");
+		jedis.set("k2","v2");
+		jedis.set("k3","v3");
+		
+		
+		System.out.println(jedis.get("k3"));
+		
+		Set<String> sets = jedis.keys("*");
+		System.out.println(sets.size());
+	}
+}
---
+++ redis-java/src/test/java/com/yufeng/redis/TestPing.java
@@ -0,0 +1,11 @@
+package com.yufeng.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class TestPing {
+	public static void main(String[] args) 
+	{
+		Jedis jedis = new Jedis("127.0.0.1",6379);
+		System.out.println(jedis.ping());
+	}
+}
