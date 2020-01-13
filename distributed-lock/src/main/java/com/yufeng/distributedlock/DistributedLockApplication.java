8e7aa0a51a3caecfb61159a039bde7ae2c92cf07
daiyu <jiangyufeng.0918@gmail.com>
2020-01-14 00:15:20 +0800

    add distribution lock

---
+++ distributed-lock/src/main/java/com/yufeng/distributedlock/DistributedLockApplication.java
@@ -0,0 +1,15 @@
+package com.yufeng.distributedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class DistributedLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedLockApplication.class, args);
+	}
+
+}
---
+++ distributed-lock/src/test/java/com/yufeng/distributedlock/DistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yufeng.distributedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
