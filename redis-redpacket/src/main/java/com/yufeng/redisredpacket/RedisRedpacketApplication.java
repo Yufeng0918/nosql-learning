eecb7bb94d6e4a874e0cd64c69b039d855bfeb05
daiyu <jiangyufeng.0918@gmail.com>
2020-01-26 17:59:15 +0800

    init

---
+++ redis-redpacket/src/main/java/com/yufeng/redisredpacket/RedisRedpacketApplication.java
@@ -0,0 +1,13 @@
+package com.yufeng.redisredpacket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisRedpacketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisRedpacketApplication.class, args);
+	}
+
+}
---
+++ redis-redpacket/src/test/java/com/yufeng/redisredpacket/RedisRedpacketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yufeng.redisredpacket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisRedpacketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
