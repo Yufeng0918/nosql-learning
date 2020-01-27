61476e456baebb8a91c39782a1a48c4c4062b372
daiyu <jiangyufeng.0918@gmail.com>
2020-01-27 21:39:09 +0800

    bloom filter init

---
+++ redis-boom-filter/src/test/java/com/yufeng/redisboomfilter/RedisBoomFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yufeng.redisboomfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisBoomFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
